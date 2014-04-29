2a3b58b471b2e735ea0311321376dc9e159a4d48
Dominionized <dominique.begin.0@gmail.com>
2014-04-29 14:17:31 -0400

    Added markdown support for news

--- TP4 - Multimedia/Nouvelles.aspx.cs
+++ TP4 - Multimedia/Nouvelles.aspx.cs
@@ -7,6 +7,7 @@ using System.Web.UI.WebControls;
 
 using System.Data.OleDb;
 using System.Configuration;
+using System.Text.RegularExpressions;
 
 namespace TP4_Multimedia
 {
@@ -22,21 +23,23 @@ namespace TP4_Multimedia
             OleDbCommand command = new OleDbCommand("SELECT nouvelles.ID, nouvelles.titre, nouvelles.contenu, nouvelles.auteur, nouvelles.date_de_creation, utilisateurs.avatar FROM nouvelles, utilisateurs WHERE utilisateurs.pseudonyme = nouvelles.auteur;", connection);
             OleDbDataReader dataReader = command.ExecuteReader();
 
+
             while (dataReader.Read())
             {
                 conteneurNouvelles.Text += "<div class=\"divNouvelle\">";
                 conteneurNouvelles.Text += "<a href\"Article.aspx?ID=" + (int)dataReader[0] + "\"><h2>" + (string)dataReader[1] + "</h2></a>";
                 conteneurNouvelles.Text += "<span class=\"date-auteur\"><img src=\"avatars/" + (string)dataReader[5] + "\"/>&nbsp;" + (string)dataReader[3] + ", le " + ((DateTime)dataReader[4]).ToLongDateString() + " à " + ((DateTime)dataReader[4]).ToShortTimeString() + ".</span><br/>";
                 conteneurNouvelles.Text += "<p>";
-                if (((string)dataReader[2]).Length > nbCaracteresMax)
+                string messageWithoutHTML = Regex.Replace((string)dataReader[2], @"<[^>]+>|&nbsp;", "").Trim();
+                if (messageWithoutHTML.Length > nbCaracteresMax)
                 {
-                    conteneurNouvelles.Text += ((string)dataReader[2]).Substring(0, nbCaracteresMax) + "...";
-                    conteneurNouvelles.Text += "<a href=\"Article.aspx?ID=" + (int)dataReader[0] + "\">Lire la suite</a>";
+                    conteneurNouvelles.Text += messageWithoutHTML.Substring(0, nbCaracteresMax) + "...";
                 }
                 else
                 {
-                    conteneurNouvelles.Text += (string)dataReader[2];
+                    conteneurNouvelles.Text += messageWithoutHTML;
                 }
+                conteneurNouvelles.Text += "<br/><a href=\"Article.aspx?ID=" + (int)dataReader[0] + "\">Lire l'article au complet</a>";
                 conteneurNouvelles.Text += "</p>";
                 conteneurNouvelles.Text += "</div>";
 
